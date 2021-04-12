56c4692f70d05c51ec8d73b9c64bc82f11fb7f2e
radharukminiY <rukku558@gmail.com>
2021-04-12 23:27:01 +0530

    commit 5

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const msg = {
   to: 'test1@gmail.com',
   from: 'test2@gmail.com',
 =======
-  to: 'radharukmini.jetzy@gmail.com',
-  from: 'gdr.sateesh12@gmail.com',
->>>>>>> f5a7703343bd42155726540540d8c3bc089b9099
+  to: 'test1@gmail.com',
+  from: 'test2@gmail.com',
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  html: '<strong>SendGrid is easy to do anywhere, even with Node.js</strong>',
 }
 sgMail
   .send(msg)
